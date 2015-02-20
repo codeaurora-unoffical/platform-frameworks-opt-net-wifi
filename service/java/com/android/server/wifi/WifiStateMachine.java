ac0bf9402374c4c74e35a6d61493f4a6b2e71850
Nalla Kartheek <karthe@codeaurora.org>
2015-02-20 14:55:21 +0530

    Wifi: Filter the scan results based on the configured band
    Framework needs to get the scan results based on the configured band as the
    supplicant / host driver shall maintain the cache for these scan results.
    Without this, the UI shall display the scan results not corresponding to the
    configured band.
    This band capability also needs to be considered while getting the visibility
    of the network to trigger auto join.
    This commit ensures the same.
    
    Change-Id: Iccc9ab936895b3fb1028c2ad6a4fa12b7d4c7067
    CRs-Fixed: 797374

--- service/java/com/android/server/wifi/WifiStateMachine.java
+++ service/java/com/android/server/wifi/WifiStateMachine.java
@@ -3424,6 +3424,17 @@ public class WifiStateMachine extends StateMachine {
                             scanResult.seen = System.currentTimeMillis();
                             mScanResultCache.put(key, scanResult);
                         }
+                        if (mFrequencyBand.get()
+                                == WifiManager.WIFI_FREQUENCY_BAND_2GHZ) {
+                            if (ScanResult.is5GHz(freq)) {
+                                continue;
+                            }
+                        } else if (mFrequencyBand.get()
+                                     == WifiManager.WIFI_FREQUENCY_BAND_5GHZ) {
+                            if (ScanResult.is24GHz(freq)) {
+                                continue;
+                            }
+                        }
                         mNumScanResultsReturned ++; // Keep track of how many scan results we got
                                                     // as part of this scan's processing
                         mScanResults.add(scanResult);
@@ -3657,7 +3668,7 @@ public class WifiStateMachine extends StateMachine {
         WifiConfiguration currentConfiguration = getCurrentWifiConfiguration();
         if (currentConfiguration != null
                 && currentConfiguration.scanResultCache != null) {
-            currentConfiguration.setVisibility(12000);
+            currentConfiguration.setVisibility(12000, mFrequencyBand.get());
             if (currentConfiguration.visibility != null) {
                 if (currentConfiguration.visibility.rssi24 != WifiConfiguration.INVALID_RSSI
                         && currentConfiguration.visibility.rssi24
@@ -5401,6 +5412,7 @@ public class WifiStateMachine extends StateMachine {
                         if (PDBG)  loge("did set frequency band " + band);
 
                         mFrequencyBand.set(band);
+                        mWifiConfigStore.setConfiguredBand(band);
                         // Flush old data - like scan results
                         mWifiNative.bssFlush();
                         if (mFrequencyBand.get() == WifiManager.WIFI_FREQUENCY_BAND_2GHZ) {
