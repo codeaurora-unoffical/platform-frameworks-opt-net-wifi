db40c09634449d22d7c049d45e33581eb1065a73
Nalla Kartheek <karthe@codeaurora.org>
2015-02-20 14:55:21 +0530

    Wifi: Filter the scan results based on the configured band
    Framework needs to get the scan results based on the configured band as the
    supplicant / host driver shall maintain the cache for these scan results.
    Without this, the UI shall display the scan results not corresponding to the
    configured band.
    This band capability also needs to be considered while getting the visibility
    of the network to trigger auto join.
    This commit ensures the same.
    
    Change-Id: Iccc9ab936895b3fb1028c2ad6a4fa12b7d4c7067
    CRs-Fixed: 797374

--- service/java/com/android/server/wifi/WifiConfigStore.java
+++ service/java/com/android/server/wifi/WifiConfigStore.java
@@ -368,6 +368,8 @@ public class WifiConfigStore extends IpConfigStore {
     public boolean enable5GHzPreference = true;
     public boolean enableWifiCellularHandoverUserTriggeredAdjustment = true;
 
+    private int mConfiguredBand = 0;
+
     /**
      * Regex pattern for extracting a connect choice.
      * Matches a strings like the following:
@@ -637,7 +639,7 @@ public class WifiConfigStore extends IpConfigStore {
             }
 
             // Calculate the RSSI for scan results that are more recent than milli
-            config.setVisibility(milli);
+            config.setVisibility(milli, mConfiguredBand);
             if (config.visibility == null) {
                 continue;
             }
@@ -1785,6 +1787,10 @@ public class WifiConfigStore extends IpConfigStore {
 
     public String getLastSelectedConfiguration() {
         return lastSelectedConfiguration;
+    }
+
+    public void setConfiguredBand(int band) {
+       mConfiguredBand = band;
     }
 
     public boolean isLastSelectedConfiguration(WifiConfiguration config) {
