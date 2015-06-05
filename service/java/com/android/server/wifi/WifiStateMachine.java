195a2d9f36843a17620517182f89b12e0a05b2e0
Srinivas Dasari <dasaris@codeaurora.org>
2015-06-05 13:34:16 +0530

    WiFi: Invoke getWifiLinkLayerStats() only if driver is loaded
    When the screen state changes from off to on,
    getWifiLinkLayerStats() is getting invoked even if the driver is
    not loaded. Add a check to invoke the same only if driver is
    loaded
    
    Change-Id: I75bbc537e076f10890a1e379161f43e885d43f4e
    CRs-Fixed: 849155

--- service/java/com/android/server/wifi/WifiStateMachine.java
+++ service/java/com/android/server/wifi/WifiStateMachine.java
@@ -3369,9 +3369,11 @@ public class WifiStateMachine extends StateMachine {
         }
         mScreenBroadcastReceived.set(true);
 
-        getWifiLinkLayerStats(false);
-        mOnTimeScreenStateChange = mOnTime;
-        lastScreenStateChangeTimeStamp = lastLinkLayerStatsUpdate;
+        if (mIsRunning) {
+            getWifiLinkLayerStats(false);
+            mOnTimeScreenStateChange = mOnTime;
+            lastScreenStateChangeTimeStamp = lastLinkLayerStatsUpdate;
+        }
         mEnableBackgroundScan = false;
         cancelDelayedScan();
 
