phhusson/platform_packages_modules_Wifi

ScanRequestProxy: Add a setting to toggle wifi scan throttling
Scan throttling affects apps which are used by testers for survey like
application. Add a hidden setting which can be toggled via adb commands
to turn off all scan throttling.

Bug: 111763749
Test: Unit tests
Test: `adb shell settings put global wifi_scan_throttle_enabled 0` on
user build.
Change-Id: Ie43550d7fad40d27e2440916b0efab19db4ff6a6

service/java/com/android/server/wifi/WifiInjector.java
@@ -257,7 +257,8 @@ public class WifiInjector {
                 (AppOpsManager) mContext.getSystemService(Context.APP_OPS_SERVICE),
                 (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE),
                 this, mWifiConfigManager,
-                mWifiPermissionsUtil, mWifiMetrics, mClock);
+                mWifiPermissionsUtil, mWifiMetrics, mClock, mFrameworkFacade,
+                new Handler(clientModeImplLooper));
         mSarManager = new SarManager(mContext, makeTelephonyManager(), clientModeImplLooper,
                 mWifiNative, new SystemSensorManager(mContext, clientModeImplLooper),
                 mWifiMetrics);

codeaurora-unoffical/platform-frameworks-opt-net-wifi

(cherry picked from commit 73cf4e7594368d0bb2688867a7c055a58bf078db)

service/java/com/android/server/wifi/WifiInjector.java
@@ -253,7 +253,8 @@ public class WifiInjector {
                 (AppOpsManager) mContext.getSystemService(Context.APP_OPS_SERVICE),
                 (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE),
                 this, mWifiConfigManager,
-                mWifiPermissionsUtil, mWifiMetrics, mClock);
+                mWifiPermissionsUtil, mWifiMetrics, mClock, mFrameworkFacade,
+                new Handler(wifiStateMachineLooper));
         mSarManager = new SarManager(mContext, makeTelephonyManager(), wifiStateMachineLooper,
                 mWifiNative, new SystemSensorManager(mContext, wifiStateMachineLooper));
         if (mUseRealLogger) {